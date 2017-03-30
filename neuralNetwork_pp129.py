7fee4458db7aa422b9357d1b17d170aa6c212892
michaelcheedecisionlab <michael.chee@decisionlab.co.uk>
2017-03-30 18:07:19 +0100

    set up initial code

---
+++ neuralNetwork_pp129.py
@@ -0,0 +1,39 @@
+'''
+Book
+Pp. 129 - 130
+'''
+
+# neural network class definition
+class neuralNetwork:
+
+    # initialise the neural network
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        # set number of nodes in each input, hidden, output layer
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+        
+        # learning rate
+        self.lr = learningrate
+        pass
+        
+    # train the neural network
+    def train():
+        pass
+        
+    # query the neural network
+    def query():
+        pass
+        
+# number of input, hidden and output nodes
+input_nodes = 3
+hidden_nodes = 3
+output_nodes = 3
+
+# learning rate is 0.3
+learning_rate = 0.3
+
+# create instance of neural network
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+  
+        
